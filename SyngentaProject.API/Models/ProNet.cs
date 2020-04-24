0f60940956abf536e53e596ea3751fdfd2e0acaf
Team_SW <software_team@vuletech.com>
2020-04-24 09:05:44 +0700

    Begin point, create APIs/v1/MasterCodes

---
+++ SyngentaProject.API/Models/ProNet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace GenagateQrCodeAPI.Models
+{
+    public class ProNet
+    {
+        //public async ReadBodyRequest()
+        //{
+        //    string result;
+        //    using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+        //    {
+        //        result = await reader.ReadToEndAsync();
+        //    }
+        //    JObject jOb = JObject.Parse(result);
+        //    result = jOb.GetValue("Password").ToString();
+        //}
+
+    }
+}
